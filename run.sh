97ae13dd085d71193205b8496ccf25d3de5846de
theodoreslechta <39388638+theodoreslechta@users.noreply.github.com>
2024-01-16 14:16:13 -0600

    Need to run within context of web server, not docker container

--- run.sh
+++ run.sh
@@ -8,16 +8,25 @@
 # $6 = group
 # $7 = octal permissions
 
-echo "Changing to webroot $3"
-cd "$3"
+echo "Here's what we've got...\n"
+echo "Domain: $1\n"
+echo "Port: $2\n"
+echo "Webroot: $3\n"
+echo "PHP Executable: $4\n"
+echo "Owner: $5\n"
+echo "Group: $6\n"
+echo "Octal Permissions: $7\n"
 
-echo "Creating the PHP file"
+echo "Creating the local PHP file"
 echo "<?php if ( function_exists( 'opcache_reset' ) ) { opcache_reset(); }" > opcache_reset.php
+
+echo "Copying to server"
+# scp ./opcache_reset.php user@target:$3/opcache_reset.php
 
 if [ -z "$5" ]
 then
     echo "User provided, running chown to $5"
-    chown $5 opcache_reset.php
+    # ssh user@target "chown $5 opcache_reset.php"
 else
     echo "Relying on default user"
 fi
@@ -25,19 +34,19 @@ fi
 if [ -z "$6" ] 
 then
     echo "Group provided, running chgrp to $6"
-    chgrp $6 opcache_reset.php
+    # ssh user@target "chgrp $6 opcache_reset.php"
 else
     echo "Relying on default group"
 fi
 
 echo "Setting permissions"
-chmod $7 opcache_reset.php
+# ssh user@target "chmod $7 opcache_reset.php"
 
 echo "Running via CLI, just in case in use"
-$4 opcache_reset.php
+# ssh user@target "$4 $3/opcache_reset.php"
 
 echo "Running via HTTP"
-curl "$1/opcache_reset.php:$2" --resolve "$1:$2:127.0.0.1"
+# ssh user@target "curl '$1/opcache_reset.php:$2' --resolve '$1:$2:127.0.0.1'"
 
 echo "Removing PHP script"
-rm opcache_reset.php
+# ssh user@target rm $3/opcache_reset.php
