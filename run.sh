406ccf0fb5bccbead8f1b8ad0aa11611ac40ccd8
theodoreslechta <39388638+theodoreslechta@users.noreply.github.com>
2024-01-17 10:45:22 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -53,7 +53,7 @@ scp -o ForwardAgent=yes -P ${10} -i repo_private_key ./opcache_reset.php $8@$9:$
 if [ -z "$5" ]
 then
     echo "User provided, running chown to $5"
-    ssh -p ${10} -i repo_private_key $8@$9 "chown $5 opcache_reset.php"
+    ssh -p ${10} -i repo_private_key $8@$9 "chown $5 $3/opcache_reset.php"
 else
     echo "Relying on default user"
 fi
@@ -61,13 +61,13 @@ fi
 if [ -z "$6" ] 
 then
     echo "Group provided, running chgrp to $6"
-    ssh -p ${10} -i repo_private_key $8@$9 "chgrp $6 opcache_reset.php"
+    ssh -p ${10} -i repo_private_key $8@$9 "chgrp $6 $3/opcache_reset.php"
 else
     echo "Relying on default group"
 fi
 
 echo "Setting permissions"
-ssh -p ${10} -i repo_private_key $8@$9 "chmod $7 opcache_reset.php"
+ssh -p ${10} -i repo_private_key $8@$9 "chmod $7 $3/opcache_reset.php"
 
 echo "Running via CLI, just in case in use"
 ssh -p ${10} -i repo_private_key $8@$9 "$4 $3/opcache_reset.php"
