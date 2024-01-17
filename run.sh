c4225c9ca0572e73e29fcd8440af547f4ec36905
theodoreslechta <39388638+theodoreslechta@users.noreply.github.com>
2024-01-17 09:32:00 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,12 +42,12 @@ echo "Creating the local PHP file"
 echo "<?php if ( function_exists( 'opcache_reset' ) ) { opcache_reset(); }" > opcache_reset.php
 
 echo "Copying to server"
-scp -P ${10} ./opcache_reset.php $8@$9:$3/opcache_reset.php
+scp -P ${10} -i repo_private_key ./opcache_reset.php $8@$9:$3/opcache_reset.php
 
 if [ -z "$5" ]
 then
     echo "User provided, running chown to $5"
-    ssh -p ${10} $8@$9 "chown $5 opcache_reset.php"
+    ssh -p ${10} -i repo_private_key $8@$9 "chown $5 opcache_reset.php"
 else
     echo "Relying on default user"
 fi
@@ -55,19 +55,19 @@ fi
 if [ -z "$6" ] 
 then
     echo "Group provided, running chgrp to $6"
-    ssh -p ${10} $8@$9 "chgrp $6 opcache_reset.php"
+    ssh -p ${10} -i repo_private_key $8@$9 "chgrp $6 opcache_reset.php"
 else
     echo "Relying on default group"
 fi
 
 echo "Setting permissions"
-ssh -p ${10} $8@$9 "chmod $7 opcache_reset.php"
+ssh -p ${10} -i repo_private_key $8@$9 "chmod $7 opcache_reset.php"
 
 echo "Running via CLI, just in case in use"
-ssh -p ${10} $8@$9 "$4 $3/opcache_reset.php"
+ssh -p ${10} -i repo_private_key $8@$9 "$4 $3/opcache_reset.php"
 
 echo "Running via HTTP"
-ssh -p ${10} $8@$9 "curl '$1/opcache_reset.php:$2' --resolve '$1:$2:127.0.0.1'"
+ssh -p ${10} -i repo_private_key $8@$9 "curl '$1/opcache_reset.php:$2' --resolve '$1:$2:127.0.0.1'"
 
 echo "Removing PHP script"
-ssh -p ${10} $8@$9 "rm $3/opcache_reset.php"
+ssh -p ${10} -i repo_private_key $8@$9 "rm $3/opcache_reset.php"
