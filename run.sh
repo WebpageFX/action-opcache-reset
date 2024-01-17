58b5fd501e39860b5508412f1cce4ef73aa50423
theodoreslechta <39388638+theodoreslechta@users.noreply.github.com>
2024-01-17 11:53:18 -0600

    Update run.sh to use named variables

--- run.sh
+++ run.sh
@@ -11,33 +11,42 @@
 # $9 = ssh port
 # ${10} = ssh key
 
+domain=$1
+webroot_path=$2
 if [ -z $3 ]
 then
     php_executable="php"
 else
     php_executable=$3
 fi
+owner=$4
+group=$5
+octal_permissions=$6
+ssh_user=$7
+ssh_host=$8
+ssh_port=$9
+ssh_key=${10}
 
 echo "Here's what we've got..."
-echo "Domain: $1"
-echo "Webroot: $2"
+echo "Domain: $domain"
+echo "Webroot: $webroot_path"
 echo "PHP Executable: $php_executable"
-echo "Owner: $4"
-echo "Group: $5"
-echo "Octal Permissions: $6"
-echo "SSH User: $7"
-echo "SSH Host: $8"
-echo "SSH Port: $9"
-echo "SSH Key: ${10}"
+echo "Owner: $owner"
+echo "Group: $group"
+echo "Octal Permissions: $octal_permissions"
+echo "SSH User: $ssh_user"
+echo "SSH Host: $ssh_host"
+echo "SSH Port: $ssh_port"
+echo "SSH Key: $ssh_key"
 
 echo "Preparing SSH..."
-echo "${10}" > repo_private_key
+echo "$ssh_key" > repo_private_key
 echo "Setting key permissions"
 #set permissions on private keys to avoid unprotected private key file error
 chmod 600 repo_private_key
 echo "Running ssh-keyscan and writing to known_hosts"
 #run ssh-keyscan to add host to known_hosts
-ssh-keyscan -p $9 -H $8 > /etc/ssh/ssh_known_hosts
+ssh-keyscan -p $ssh_port -H $ssh_host > /etc/ssh/ssh_known_hosts
 echo "Preparing SSH agent forwards..."
 eval $(ssh-agent -s)
 echo "Adding key to SSH"
@@ -48,35 +57,35 @@ echo "Creating the local PHP file"
 echo "<?php if ( function_exists( 'opcache_reset' ) ) { opcache_reset(); echo 'Opcache Reset!'; } else { echo 'Failed to reset opcache'; }" > opcache_reset.php
 
 echo "Copying to server"
-scp -o ForwardAgent=yes -P $9 -i repo_private_key ./opcache_reset.php $7@$8:$2/opcache_reset.php
+scp -o ForwardAgent=yes -P $ssh_port -i repo_private_key ./opcache_reset.php $ssh_user@$ssh_host:$webroot_path/opcache_reset.php
 
-if [ -z "$4" ]
+if [ -z "$owner" ]
 then
-    echo "User provided, running chown to $4"
-    ssh -p $9 -i repo_private_key $7@$8 "chown $4 $2/opcache_reset.php"
+    echo "User provided, running chown to $owner"
+    ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "chown $owner $webroot_path/opcache_reset.php"
 else
     echo "Relying on default user"
 fi
 
-if [ -z "$5" ] 
+if [ -z "$group" ] 
 then
-    echo "Group provided, running chgrp to $5"
-    ssh -p $9 -i repo_private_key $7@$8 "chgrp $5 $2/opcache_reset.php"
+    echo "Group provided, running chgrp to $group"
+    ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "chgrp $group $webroot_path/opcache_reset.php"
 else
     echo "Relying on default group"
 fi
 
 echo "Setting permissions"
-ssh -p $9 -i repo_private_key $7@$8 "chmod $6 $2/opcache_reset.php"
+ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "chmod $octal_permissions $webroot_path/opcache_reset.php"
 
 echo "Running via CLI, just in case in use"
-ssh -p $9 -i repo_private_key $7@$8 "$php_executable $2/opcache_reset.php"
+ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "$php_executable $webroot_path/opcache_reset.php"
 
 echo "Running via HTTP"
-echo "URL: $1/opcache_reset.php"
-result=$(ssh -p $9 -i repo_private_key $7@$8 "curl '$1/opcache_reset.php' --resolve '$1:127.0.0.1'")
+echo "URL: $domain/opcache_reset.php"
+result=$(ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "curl '$domain/opcache_reset.php' --resolve '$domain:127.0.0.1'")
 
 echo "Result: $result"
 
 echo "Removing PHP script"
-ssh -p $9 -i repo_private_key $7@$8 "rm $2/opcache_reset.php"
+ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "rm $webroot_path/opcache_reset.php"
