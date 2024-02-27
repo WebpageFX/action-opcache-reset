5412bece97a433b5fe4e87d17f610d92b0405caf
Andrew Dawes <andrew@webfx.com>
2024-02-27 14:18:52 -0500

    Add retries

--- run.sh
+++ run.sh
@@ -15,6 +15,8 @@ ssh_user=$7
 ssh_host=$8
 ssh_port=$9
 ssh_key=${10}
+retries_opcache_reset_http=${11}
+
 
 echo "Here's what we've got..."
 echo "Domain: $domain"
@@ -27,6 +29,7 @@ echo "SSH User: $ssh_user"
 echo "SSH Host: $ssh_host"
 echo "SSH Port: $ssh_port"
 echo "SSH Key: $ssh_key"
+echo "Retries: $retries_opcache_reset_http"
 
 echo "Preparing SSH..."
 echo "$ssh_key" > repo_private_key
@@ -71,28 +74,45 @@ echo "Running via CLI, just in case in use"
 cli_result=$(ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "$php_executable $webroot_path/opcache_reset.php")
 cli_status=$?
 
-echo "Running via HTTP"
-echo "URL: $domain/opcache_reset.php"
-http_result=$(ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "curl '$domain/opcache_reset.php' --resolve '$domain:127.0.0.1'")
-http_status=$?
 
 # We haven't encountered a situation where the CLI using opcache was an issue, so if it fails, it's *probably* not the end of the world and not worth failing the job
 echo "CLI Result: $cli_result"
 echo "CLI Status: $cli_status"
 # If HTTP gives us the failure message, let's fail the job
+
+exit_code=0
+echo "Running via HTTP"
+echo "URL: $domain/opcache_reset.php"
+opcache_reset_http() {
+    http_result=$(ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "curl '$domain/opcache_reset.php' --resolve '$domain:127.0.0.1'")
+    http_status=$?
+    if [ "$http_result" = 'Failed to reset opcache' ]; then
+        echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
+        exit_code=1
+    elif [ "$http_result" = 'No input file specified.' ]; then
+        echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
+        exit_code=1
+    elif [ $http_status -ne 0 ]; then
+        echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
+        exit_code=1
+    fi
+    echo "HTTP Result: $http_result"
+    return $exit_code
+}
+# Retry the HTTP request if it fails
+for i in $(seq 1 "$retries_opcache_reset_http"); do
+    echo "Retrying HTTP request $i of $retries_opcache_reset_http"
+    # Capture the result and exit code of the function
+    http_result=$(opcache_reset_http)
+    # If the function exits with a 0 status code, we're good
+    http_status=$?
+    if [ $http_status -eq 0 ]; then
+        break
+    fi
+done
+
 echo "HTTP Result: $http_result"
 echo "HTTP Status: $http_status"
-exit_code=0
-if [[ "$http_result" == 'Failed to reset opcache' ]]; then
-    echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
-    exit_code=1
-elif [[ "$http_result" == 'No input file specified.' ]]; then
-    echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
-    exit_code=1
-elif [ $http_status -ne 0 ]; then
-    echo "FAILED TO RESET OPCACHE. RESET MANUALLY FOR CHANGES TO TAKE EFFECT"
-    exit_code=1
-fi
 
 echo "Removing PHP script"
 ssh -p $ssh_port -i repo_private_key $ssh_user@$ssh_host "rm $webroot_path/opcache_reset.php"
